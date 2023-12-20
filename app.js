d6286deb2096f2e456f572206961c175c43ccbc2
VitorBraz <vitordejesus2017@outlook.com>
2023-12-20 19:54:15 -0300

    Adiciona endpoint /webhook no servidor Express
    Esta alteração implementa um novo endpoint no servidor Express para receber
    dados do N8N por meio de um webhook. Foi adicionada uma rota POST /webhook
    que processa os dados recebidos e exibe no console para fins de teste. Esta
    funcionalidade é essencial para integrar o N8N ao servidor Node.js.

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser');
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+const app = express();
+const PORT = 3000; // Escolha a porta desejada
+
+app.use(bodyParser.json());
+
+// Rota para receber os dados do N8N
+app.post('/webhook', (req, res) => {
+  try {
+    // Extraia os dados do corpo da solicitação
+    const data = req.body[0];
+
+    // Exiba os dados no console (apenas para teste)
+    console.log('Dados recebidos:', data);
+
+    res.status(200).json({ message: 'Webhook testado com sucesso!' });
+  } catch (error) {
+    console.error('Erro ao processar o webhook:', error);
+    res.status(500).json({ error: 'Erro interno do servidor' });
+  }
 });
 
-const port = 3000;
-app.listen(port, () => {
-  console.log(`Servidor rodando em http://localhost:${port}`);
+// Inicie o servidor
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
---
+++ config.js
@@ -0,0 +1,8 @@
+// config.js
+module.exports = {
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'roobotsjobsdb', // Certifique-se de que o nome corresponde ao banco de dados criado
+  };
+  
